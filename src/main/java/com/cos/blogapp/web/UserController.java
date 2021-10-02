75a8ba23fc99837fdf9c51d2447e68e21308aa39
dola258 <dola258@naver.com>
2021-10-02 11:22:59 +0900

    홈페이지 헤더/풋 include

---
+++ src/main/java/com/cos/blogapp/web/UserController.java
@@ -0,0 +1,37 @@
+package com.cos.blogapp.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class UserController {
+	
+	@GetMapping("/home")
+	public String home() {
+		return "home";
+	}
+	
+	// /WEB-INF/views/user/login.jsp
+	@GetMapping("/loginForm")
+	public String login() {
+		return "user/loginForm";
+	}
+	
+	@GetMapping("/joinForm")
+	public String join() {
+		return "user/joinForm";
+	}
+	
+	@PostMapping("/login")
+	public String login(String username, String password) {
+		// 1. username, password 받기
+		System.out.println(username);
+		System.out.println(password);
+		// 2. DB 조회
+		// 3. 있으면
+		// 4. session에 저장
+		// 5. 메인페이지를 돌려주기
+		return "home";
+	}
+}
---
+++ src/main/webapp/WEB-INF/views/layout/footer.jsp
@@ -0,0 +1,12 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+
+<div class="jumbotron text-center" style="margin-bottom:0">
+  <p>이효빈</p>
+  <p>📞010-1111-1111</p>
+  <p>🚘부산시 북구 xxxx</p>
+  <p>🏴‍☠️사업자번호 : 000-0000-0000</p>
+</div>
+
+</body>
+</html>
