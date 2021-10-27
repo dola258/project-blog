198f4445b77d231d57c4fc334611b48e7e7b2d04
dola258 <dola258@naver.com>
2021-10-27 13:50:43 +0900

    커스텀 익셉션

---
+++ src/main/java/com/cos/blogapp/handler/exception/MyNotFoundException.java
@@ -0,0 +1,14 @@
+package com.cos.blogapp.handler.exception;
+
+/**
+ * 
+ * @author 이효빈 2021.09.16
+ * 1. id를 못찾았을 때 사용
+ * 
+ */
+public class MyNotFoundException extends RuntimeException{
+
+	public MyNotFoundException(String msg) {
+		super(msg);
+	}
+}
